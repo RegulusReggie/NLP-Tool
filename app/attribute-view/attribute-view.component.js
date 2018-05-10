07015f827fea38cbb9536affc20ace92385f72da
RegulusReggie <wrhuang@ucla.edu>
2018-05-10 12:33:39 -0700

    progress update 1

--- app/attribute-view/attribute-view.component.js
+++ app/attribute-view/attribute-view.component.js
@@ -1,11 +1,50 @@
 // Define 'attribute-view' component
+
+const attrMap = {
+	attr_level1: {
+		1: '疾病、表征、生理状态',
+		2: '处理、治疗办法与效果',
+		3: '周围环境、条件与活动'
+	},
+	attr_level2: {
+		1: '疾病/皮损/生理状态',
+		2: '过敏史/过敏原',
+		3: '身体部位',
+		4: '自觉症状',
+		5: '皮损大小',
+		6: '皮损整体分布',
+		7: '皮损形态形状',
+		8: '皮损颜色',
+		9: '病患/症状出现时间',
+		10: '病患严重程度和变化'
+	}
+}
+
 function AttributeViewController() {
 	var self = this;
-	self.update = function(attr, value) {
+	self.attrMap = attrMap;
+	self.update = function($event, attr, value) {
+		var thisButton = $event.currentTarget;
+		var current = thisButton.parentNode.getElementsByClassName("active");
+		if (current.length > 0) {
+			current[0].className = current[0].className.replace(" active", "");	
+		}
+		
+		thisButton.className += " active";
 		self.onUpdate({entity: self.entity, attr: attr, value: value});
 	}
-	self.$onChanges = function(changesObj) {
-		//console.log(self.entity)
+	self.isActiveButton = function(attr, value) {
+		return self.entity 
+			&& self.entity.attributes
+			&& self.entity.attributes[attr] == value
+			? "active"
+			: ""
+	}
+	self.isHidden = function() {
+		return self.entity
+			&& self.entity.attributes
+			? ""
+			: "hidden"
 	}
 }
 
